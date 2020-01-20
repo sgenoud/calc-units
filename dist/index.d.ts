44f716868d8460b3039b3590c699a73a2378fa59
Steve Genoud <steve@sgenoud.com>
2020-01-20 10:30:41 +0100

    v0.2.1

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const CALC_EXPR: RegExp;
-export declare const isCalc: (calcExpr: string) => boolean;
-export default function calcUnits(calcExpr: string, parseValue: (n: string) => number): number;
